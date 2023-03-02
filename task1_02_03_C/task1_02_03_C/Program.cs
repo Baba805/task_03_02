fc2fb83ef261b135dab69d932b048948ed78bfa1
user name <you@example.com>
2023-03-02 07:05:19 +0400

    first commit

---
+++ task1_02_03_C/task1_02_03_C/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1_02_03_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            int[] arr = { 13, 24, 41, 65, 78 };
+            int cem = 0;
+            for(int i = 0; i< arr.Length; i++)
+            {
+                if(arr[i] % 2 != 0)
+                {
+                    cem += arr[i];
+                }
+
+            }
+            Console.WriteLine(cem);
+
+        }
+    }
+}
---
+++ task2_02_03_C/task2_02_03_C/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task2_02_03_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.Arraydaki ədədlərin ədədi ortasını hesablayan alqoritm qurun.
+
+            int[] arr = { 12, 25, 88, 71, 90 };
+            int cem = 0;
+            int say = 0;
+            for(int i = 0; i<arr.Length; i++)
+            {
+                cem += arr[i];
+                say++;
+            }
+            Console.WriteLine(cem / say);
+        }
+    }
+}
